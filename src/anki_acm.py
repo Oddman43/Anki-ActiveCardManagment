6903b8863c92b941a7d79356e8cab7b2d7839bef
Albert Sevilleja <albert.sevilleja@gmail.com>
2025-02-23 10:44:29 +0100

    added funct to create tags list

--- src/anki_acm.py
+++ src/anki_acm.py
@@ -10,7 +10,7 @@ def get_card_info(deck_name: str, min_reviews: int = 3, ) -> list:
 
     :param deck_name: The name of the Anki deck. Spaces in the name should be escaped (e.g., "My Deck" becomes "My%20Deck")
     :param min_reviews: The minimum number of reviews a card must have to be included in the results. Defaults to 3.    
-    :return: A list of Anki_Card objects containing information about the retrived cards
+    :return: A list of Anki_Card objects containing information about the retrived cards in the specified deck
     """
     info_review: dict = {
         k: v
@@ -34,6 +34,25 @@ def get_card_info(deck_name: str, min_reviews: int = 3, ) -> list:
     return cards
 
 
+def generate_tags(increments: int = 10, tag_name: str = "AnkiACM", floor: int = 60) -> list:
+    """Generates a list containing the name of tags
+
+    Uses correct syntax so Anki connect and Anki can interpret correctly the tag
+
+    :param increments: Defaults 10
+    :param tag_name: Defaults AnkiACM
+    :param floor: Defaults 60
+    :return: List of tag names
+    """
+    percentages: list = [x for x in range(floor, 101, increments)]
+    tag_list: list = [f"{tag_name}::<{floor}%",]
+    i: int = 1
+    for i in range(len(percentages)):
+        tag_list.append(f"{tag_name}::{percentages[i-1]}-{percentages[i]}%")
+    tag_list.append(f"{tag_name}::100%")
+    return tag_list
+
+
 def update_tags(tags_list: list, cards: list[Anki_Card]) -> None:
     """Computes the correct tag and updates the object
 
@@ -50,9 +69,9 @@ def update_tags(tags_list: list, cards: list[Anki_Card]) -> None:
 
 
 def commit_update_tags(cards: list[Anki_Card]) -> None:
-    """Updates on Anki using Anki Connect the Cards
+    """Updates the Anki tags using Anki Connect
 
-    :param cards: A list of Anki_Cards
+    :param cards: A list of Anki_Card objects
     """
     for card in cards:
         updated_tags: dict = card.format_update_tags()
@@ -60,16 +79,8 @@ def commit_update_tags(cards: list[Anki_Card]) -> None:
 
 
 def main() -> None:
-    # Hacer que las tags las genere con list comprehension apartir de los niveles seleccionados
-    tags: list[str] = [
-        "AnkiACM::<60%",
-        "AnkiACM::60%-70%",
-        "AnkiACM::70%-80%",
-        "AnkiACM::80%-90%",
-        "AnkiACM::90%-99%",
-        "AnkiACM::100%",
-    ]
-    decks: list = ["",]
+    tags: list[str] = generate_tags(increments= 10, tag_name= "AnkiACM", floor = 60)
+    decks: list = ["Change to deck name",]
     for deck in decks:
         print(f"Getting card info for {deck}")
         cards: list[Anki_Card] = get_card_info(deck)
