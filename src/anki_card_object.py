780324d49e37d0364b28b4531e28598c308fba09
Albert Sevilleja <albert.sevilleja@gmail.com>
2025-02-23 11:23:55 +0100

    updated

--- src/anki_card_object.py
+++ src/anki_card_object.py
@@ -13,22 +13,17 @@ class Anki_Card:
         self.compute_percentaje()
 
     def compute_percentaje(self):
-        if len(self.reviews) < 2:
+        if len(self.reviews) < 3:
             self.percentage = 0.0
             return
         reviews_to_process: list[dict] = (
             self.reviews[-10:] if len(self.reviews) > 10 else self.reviews
         )
-        subs_dict_0 = {1: 0.5, 2: 0.2, 3: 1.0, 4: 1.2, 0: 0}
         subs_dict = {1: 0, 2: 0.2, 3: 1.0, 4: 1.2, 0: 0}
         rescheduled = any(dic["ease"] == 0 for dic in reviews_to_process)
-        if len(reviews_to_process) < 10:
-            numerator = subs_dict_0[reviews_to_process[0]["ease"]]
-            numerator += sum(subs_dict[dic["ease"]] for dic in reviews_to_process[1:])
-        else:
-            numerator = sum(
-                subs_dict[dic["ease"]] for dic in reviews_to_process[-10:]
-            )
+        numerator = sum(
+            subs_dict[dic["ease"]] for dic in reviews_to_process[-10:]
+        )
         denominator = (
             len(reviews_to_process) - 1 if rescheduled else len(reviews_to_process)
         )
