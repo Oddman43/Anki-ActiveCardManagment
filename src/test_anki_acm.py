2e1f1dda75d00f0cc36e8cc5a2881456a01c85d1
Albert Sevilleja <albert.sevilleja@gmail.com>
2025-02-23 12:01:16 +0100

    added doc strings

--- src/test_anki_acm.py
+++ src/test_anki_acm.py
@@ -5,13 +5,20 @@ from test_cards_fixture import cards_variations
 
 
 def test_percentage(cards_variations: list[Anki_Card]) -> None:
+    """Tests that the `__post_init__` method `compute_percentaje` works as intended
+
+    :param cards_variations: Pyests fixture of a list containing Anki_Card objects 
+    """
     percentages = [0.32, 0.64, 0.74, 0.84, 0.92, 1.0, 0.58, 0.64, 1.0]
     for i, card in enumerate(cards_variations):
         assert card.percentage == pytest.approx(percentages[i], rel=1e-2)
 
 
 def test_update_tag(cards_variations: list[Anki_Card]) -> None:
-    tags: list[str] = generate_tags()
+    """Tests that the tags are updated or added correctly
+
+    :param cards_variations: Pyests fixture of a list containing Anki_Card objects 
+    """
     expected_tags: list = [
         "AnkiACM::<60%",
         "AnkiACM::60%-70%",
@@ -23,12 +30,14 @@ def test_update_tag(cards_variations: list[Anki_Card]) -> None:
         "AnkiACM::60%-70%",
         "AnkiACM::100%",
     ]
-    update_tags(tags, cards_variations)
+    update_tags(generate_tags(), cards_variations)
     for i, card in enumerate(cards_variations):
         assert expected_tags[i] in card.tags
 
 
 def test_generate_tags() -> None:
+    """Tests generate_tags function with a wide range of inputs
+    """
     tags_expected: list = [
         [
             "AnkiACM::<60%",
@@ -87,7 +96,9 @@ def test_generate_tags() -> None:
         assert tags_expected[i] == generate_tags(*tags_params[i])
 
 
-def test_format_dict(cards_variations: Anki_Card) -> None:
+def test_format_update_tags(cards_variations: Anki_Card) -> None:
+    """Tests `format_update_tags` method
+    """
     update_tags(generate_tags(), [cards_variations[0], cards_variations[7]])
     card_1: Anki_Card = cards_variations[0]
     card_2: Anki_Card = cards_variations[7]
